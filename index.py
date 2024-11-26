1d2a520997c9839af67febe83ba8e0e685239df7
olivia-s-a <otrabalhoeschool@gmail.com>
2024-11-26 13:07:15 -0300

    Fazer selectbox do tópico 1

--- index.py
+++ index.py
@@ -1,39 +1,64 @@
 import streamlit as st
+import geopandas as gpd
+import pandas as pd
+from os.path import join
+
+# Carregando dados
+distrito = gpd.read_file(join("data", "2024_11_26", "03_consumo_distrito"))
+subbac = gpd.read_file(join("data", "2024_11_26", "03_consumo_subbac"))
+subpref = gpd.read_file(join("data", "2024_11_26", "03_consumo_subprefeitura"))
+fcu = gpd.read_file(join("data", "2024_11_26", "pop_fcu"))
+#https://ftp.ibge.gov.br/Censos/Censo_Demografico_2022/Agregados_por_Setores_Censitarios_preliminares/malha_com_atributos/setores/shp/UF/SP/SP_Malha_Preliminar_2022.zip
 
 # Cabeçalho com bolinhas
 
 # Título
 st.title("Dados de Abastecimento de Água")
-st.subheader("Metodologia de análise dos dados | PMSB 2024 | CODATA")
+st.header("Metodologia de análise dos dados | PMSB 2024 | CODATA")
 st.text("Este material tem por objetivo registrar a metodologia referente ao processamento de dados elaborado por Codata para a elaboração do diagnóstico do Plano Municipal de Saneamento Básico (2024/2025). Nesse sentido, ele deve ser resultado de um processo enquanto as análises estão sendo realizadas.")
 
-# Listagem com bolinhas azuis e o primeiro item
+
+#testes
+st.dataframe(distrito)
+
+# 1: Cálculo populacional e de domicílios com base no Censo 2022
 st.markdown("""
     <ul>
         <li style="font-size: 18px; display: flex; align-items: center;">
             <div style="width: 30px; height: 30px; background-color: #3498db; border-radius: 50%; color: white; display: flex; justify-content: center; align-items: center; margin-right: 10px;">1</div>
-            <h4>Cálculo populacional e de domicílios com base no Censo 2022</h4>
+            <h3>Cálculo populacional e de domicílios com base no Censo 2022</h3>
         </li>
     </ul>
 """, unsafe_allow_html=True)
 
 st.markdown("<h5>Desagregado por</h5>", unsafe_allow_html=True)
 
-# Criando as colunas
-col1, col2, col3 = st.columns(3)
+itens = [
+    ("Subprefeituras", "Lorem ipsum dolor sit amet...", 'subpref'),
+    ("Distritos", "Lorem ipsum dolor sit amet...", 'distrito'),
+    ("Favelas e Comunidades Urbanas", "Lorem ipsum dolor sit amet...", 'fcu'),
+    ("Sub Bacias Hidrográficas", "Lorem ipsum dolor sit amet...", 'subbac')
+]
 
-# Abrindo a lista ordenada antes das colunas
+num_colunas = len(itens)
+colunas = st.columns(num_colunas)  
 
-# futuramente arrumar esse 1... o ideal era que fosse um <ol><li>
-with col1:
-    st.markdown("<p><strong>1. Distrito</strong><br> Lorem ipsum dolor sit amet...</p>", unsafe_allow_html=True) 
-with col2:
-    st.markdown("<p><strong>2. Sub bacias</strong><br>Lorem ipsum dolor sit amet...</p>", unsafe_allow_html=True)
-with col3:
-    st.markdown("<p><strong>3. Setor censitário</strong> <br> Lorem ipsum dolor sit amet...</p>", unsafe_allow_html=True)
+for i, item in enumerate(itens):
+    col = colunas[i]  
+    with col:
+        st.markdown(f"<p><strong>{i + 1}. {item[0]}</strong><br> {item[1]}</p>", unsafe_allow_html=True)
 
-# Fechando a lista ordenada após as colunas
-st.markdown("</ol>", unsafe_allow_html=True)
+
+sum_mun = distrito['pop_total'].sum()
+st.markdown("<h5>Total do Município</h5>", unsafe_allow_html=True)
+st.subheader(f'{sum_mun} pessoas')
+
+unidades = pd.DataFrame(itens, columns=['name', 'desc', 'gdf_name'])
+choice_unidade = st.selectbox("", unidades['name'])
+
+
+
+
 
 
 # 2. Demanda da População por água
@@ -50,19 +75,3 @@ st.markdown("""
 st.markdown("<h5>Desagregado por</h5>", unsafe_allow_html=True)
 
 # Lista de itens
-itens = [
-    ("Distrito", "Lorem ipsum dolor sit amet..."),
-    ("Sub bacias", "Lorem ipsum dolor sit amet..."),
-    ("Setor censitário", "Lorem ipsum dolor sit amet..."),
-    ("Assentamentos Precários", "Lorem ipsum dolor sit amet...")
-]
-
-# Definindo o número de colunas baseado no número de itens
-num_colunas = len(itens)
-colunas = st.columns(num_colunas)  # Gera automaticamente a quantidade de colunas conforme o número de itens
-
-# Gerando os itens nas colunas
-for i, item in enumerate(itens):
-    col = colunas[i]  # Acessando dinamicamente a coluna correspondente
-    with col:
-        st.markdown(f"<p><strong>{i + 1}. {item[0]}</strong><br> {item[1]}</p>", unsafe_allow_html=True)
